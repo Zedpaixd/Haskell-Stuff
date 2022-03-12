7b9a1aa56f2fd7d0c224e6b42c80483a5c875860
Armand Alexandru Balint <70021545+Zedpaixd@users.noreply.github.com>
2022-03-12 15:03:02 +0200

    Fixed issue caused by poor variable renaming

--- BST.hs
+++ BST.hs
@@ -37,9 +37,9 @@ search (Nod currentVal leftTree rightTree) searchedVal
 insBST :: Tree -> Integer -> Tree
 insBST Leaf currentVal = Nod currentVal Leaf Leaf
 insBST (Nod currentVal leftTree rightTree) insertedVal
-    | currentVal < insertedVal  = Nod currentVal (insBST leftTree insertedVal) rightTree
+    | currentVal > insertedVal  = Nod currentVal (insBST leftTree insertedVal) rightTree
     | currentVal == insertedVal = Nod currentVal leftTree rightTree
-    | currentVal > insertedVal  = Nod currentVal leftTree (insBST rightTree insertedVal)
+    | currentVal < insertedVal  = Nod currentVal leftTree (insBST rightTree insertedVal)
 
 
 main :: IO ()
